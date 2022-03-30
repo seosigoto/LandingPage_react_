97fad8c9dd348a4b8c6583b9df6af2966c305027
BCDev729 <superdev729@gmail.com>
2022-03-31 08:13:36 +0900

    fix some feedback

--- src/Page/Header.js
+++ src/Page/Header.js
@@ -2,11 +2,12 @@ import React from "react";
 import Discord from '../assets/Discord Icon.svg'
 import Twitter from '../assets/Twitter Icon.svg'
 import Medium from '../assets/Medium Icon.svg'
+import DoubleLogo from '../assets/Double Logo.svg'
 export function Header() {
     return (
         <div className="header-section">
             <div className="logo">
-                !!
+                <img src={DoubleLogo} alt="double"/>
             </div>
             <div className="social-icons">
                 <a href="https://discord.com" target="_blank"><img src={Discord} alt='discord'/></a>
